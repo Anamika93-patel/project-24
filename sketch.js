2fc3ff55653945f4bb94b3f1f4282b3d8356062c
Anamika93-patel <66108521+Anamika93-patel@users.noreply.github.com>
2020-08-03 21:16:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -66,7 +66,7 @@ function draw() {
 
 function keyPressed() {
  if (keyCode === UP_ARROW) {
-    Matter.Body.applyForce(paper1.body,paper1.body.position,{x:85,y:-85});
+    Matter.Body.applyForce(paper1.body,paper1.body.position,{x:90,y:-90});
     
   }
 }
